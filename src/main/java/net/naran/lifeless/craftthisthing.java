9bf33c260135a932f662094f5c9f56efee140e02
नारायण भुसाल <iamnarayan02@gmail.com>
2025-04-20 23:29:06 +0545

    fun adding alexandrite

--- src/main/java/net/naran/lifeless/craftthisthing.java
+++ src/main/java/net/naran/lifeless/craftthisthing.java
@@ -1,6 +1,7 @@
 package net.naran.lifeless;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,6 +16,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.naran.lifeless.item.Moditems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -37,6 +39,8 @@ public class craftthisthing
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        Moditems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,21 +49,15 @@ public class craftthisthing
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
-    }
+    {  }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(Moditems.ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/naran/lifeless/item/Moditems.java
@@ -0,0 +1,29 @@
+package net.naran.lifeless.item;
+
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.naran.lifeless.craftthisthing;
+
+public class Moditems {
+public static final DeferredRegister<Item> ITEMS=
+        DeferredRegister.create(ForgeRegistries.ITEMS, craftthisthing.MOD_ID);
+
+
+
+
+public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite" ,
+            () -> new Item(new Item.Properties()));
+
+
+
+
+public static void register(IEventBus eventBus) {
+    ITEMS.register(eventBus);
+}
+
+
+}
